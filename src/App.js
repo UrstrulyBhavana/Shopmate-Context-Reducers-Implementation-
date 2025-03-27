e4df3465122a111d48318b2653027318d8e885e0
bhavana <urstrulybhavana1432@gmail.com>
2025-03-27 16:29:20 +0530

    Integrating Context and Reducer

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
+import { Footer } from "./components/Footer";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
